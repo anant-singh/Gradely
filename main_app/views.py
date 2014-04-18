ba39a53e6a1416327d654887c8081d4c7e2222e9
Neelabh Gupta <gupta.neelabh@gmail.com>
2014-04-18 08:37:04 -0500

    Created main layout by refactoring HTML

--- main_app/views.py
+++ main_app/views.py
@@ -11,27 +11,43 @@ from django.shortcuts import render
 # Create your views here.
 @login_required()
 def home(request):
+    """
+    Show the home page.
+    """
     return render(request, 'main_app/profile.html', {})
 
 
 def login_view(request):
-    context = {}
+    """
+    Show the login form.
+    """
+    context = {"next": request.GET.get('next', '/')}
     context.update(csrf(request))
     return render(request, 'main_app/login.html', context)
 
 
 def register_view(request):
+    """
+    Register the user if a POST request was made, or show the registration form.
+    """
     if request.user.is_authenticated():
         return HttpResponseRedirect('/')
     if request.method == 'POST':
-        user = User.objects.create_user(
-            username=request.POST.get('username', ''),
-            password=request.POST.get('password', ''),
-            email=request.POST.get('email'),
-            first_name=request.POST.get('firstname'),
-            last_name=request.POST.get('lastname')
-        )
-        return HttpResponseRedirect('/')
+        try:
+            user = User.objects.create_user(
+                username=request.POST.get('username', ''),
+                password=request.POST.get('password', ''),
+                email=request.POST.get('email'),
+                first_name=request.POST.get('firstname'),
+                last_name=request.POST.get('lastname')
+            )
+            if user is not None:
+                response = {'status': 'true'}
+            else:
+                response = {'status': 'false', 'reason': 'Error while creating new user.'}
+        except Exception as e:
+            response = {'status': 'false', 'reason': 'Error while creating new user: ' + str(e)}
+        return HttpResponse(json.dumps(response, cls=DjangoJSONEncoder), content_type='application/json')
     else:
         context = {}
         context.update(csrf(request))
@@ -39,11 +55,17 @@ def register_view(request):
 
 
 def logout_view(request):
+    """
+    Log out the user.
+    """
     logout(request)
     return HttpResponseRedirect('/')
 
 
 def auth_view(request):
+    """
+    Authenticates the user.
+    """
     username = request.POST.get('username', '')
     password = request.POST.get('password', '')
     user = authenticate(username=username, password=password)
